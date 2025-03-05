e9cb54865926eb7057d31b27befe52c7561903b5
Arthit Suriyawongkul <arthit@gmail.com>
2025-03-05 13:12:28 +0700

    Add property existence test
    Signed-off-by: Arthit Suriyawongkul <arthit@gmail.com>

--- tests/test_import.py
+++ tests/test_import.py
@@ -1,6 +1,6 @@
-#
+# SPDX_License-FileType: SOURCE
 # SPDX-License-Identifier: Apache-2.0
-#
+
 
 def test_import():
     import spdx_python_model
@@ -18,3 +18,27 @@ def test_version():
     from spdx_python_model import VERSION
 
     print(VERSION)
+
+
+def test_property_exist():
+    from spdx_python_model import v3_0_1
+
+    # "imports" replaced by "import" in 3.0.1
+    d = v3_0_1.SpdxDocument()
+    assert hasattr(
+        d, "import_"
+    ), "'import_' (import) attribute is missing from 'SpdxDocument'"
+
+    # "parameters" replaced by "parameter" in 3.0.1
+    b = v3_0_1.build_Build()
+    assert hasattr(
+        b, "build_parameter"
+    ), "'build_parameter' attribute is missing from 'build_Build'"
+    assert not hasattr(
+        b, "build_parameters"
+    ), "'build_parameters' attribute should not be presented in 'build_Build'"
+
+    p = v3_0_1.ai_AIPackage()
+    assert hasattr(
+        p, "ai_domain"
+    ), "'ai_domain' attribute is missing from 'ai_AIPackage'"
